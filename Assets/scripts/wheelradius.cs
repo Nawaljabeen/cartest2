92c67644bf8ef12a752fece158216e5daa59ca00
Nawaljabeen <jabeennawal@gmail.com>
2025-06-25 01:13:12 +0500

    g

---
+++ Assets/scripts/wheelradius.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wradius : MonoBehaviour
+{
+    public MeshRenderer wheelMeshRenderer;
+    void Start()
+    {
+        if (wheelMeshRenderer != null)
+        {
+            // Y-axis is vertical � adjust if needed
+            float radius = wheelMeshRenderer.bounds.extents.y;
+            Debug.Log("Wheel radius: " + radius + " units");
+        }
+        else
+        {
+            Debug.LogError("Wheel Mesh Renderer is not assigned.");
+        }
+    }
+}
